c5baa79dd77ec7aa1bc18d61dde315555b63bb7e
bhupesh <bhupesh@webnexus.in>
2025-09-29 15:30:18 +0530

    sixth commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DIRECT_URL;
 const prisma = new PrismaClient();
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.send('app is running');
 })
 
